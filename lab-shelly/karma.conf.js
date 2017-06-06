a4b6ece63febad84ef98d48b537d129942c34d83
Shelly Tang <shellytang@gmail.com>
2017-06-05 17:38:10 -0700

    lab scaffold

---
+++ lab-shelly/karma.conf.js
@@ -0,0 +1,35 @@
+// Karma configuration
+// Generated on Mon Jun 05 2017 17:32:55 GMT-0700 (PDT)
+'use strict';
+
+const webpack = require('./webpack.config.js');
+delete webpack.entry;
+
+module.exports = function(config) {
+  config.set({
+    webpack,
+    basePath: '',
+    frameworks: ['mocha'],
+    files: [
+      'node_modules/babel-polyfill/dist/polyfill.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'app/entry.js',
+      'test/**/*-test.js',
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+      'app/entry.js': ['webpack'],
+      'test/**/*-test.js': ['webpack'],
+    },
+    reporters: ['mocha'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity,
+  });
+};
