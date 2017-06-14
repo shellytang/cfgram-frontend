fca8245e18e05b7f66210c9c40814bc378f03995
Shelly Tang <shellytang@gmail.com>
2017-06-14 12:39:45 -0700

    installed angular bootstrap and updated entry file

--- lab-shelly/app/entry.js
+++ lab-shelly/app/entry.js
@@ -6,10 +6,13 @@ const path = require('path');
 const camelcase = require('camelcase');
 const pascalcase = require('pascalcase');
 const angular = require('angular');
-require('ng-file-upload');
+const ngTouch = require('angular-touch');
+const ngAnimate = require('angular-animate');
 require('@uirouter/angularjs');
+require('angular-ui-bootstrap');
+require('ng-file-upload');
 
-const cfgram = angular.module('cfgram', ['ui.router', 'ngFileUpload']);
+const cfgram = angular.module('cfgram', ['ui.router', 'ngFileUpload', 'ngTouch', 'ngAnimate', 'ui.bootstrap']);
 
 let context = require.context('./config/', true, /\.js$/);
 context.keys().forEach( path => cfgram.config(context(path)));
