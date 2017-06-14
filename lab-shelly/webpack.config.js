4e781603cb482d789ce71c947c963b2596d79d95
Shelly Tang <shellytang@gmail.com>
2017-06-14 14:40:06 -0700

    temp fix for deploy bug covered in class

--- lab-shelly/webpack.config.js
+++ lab-shelly/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        // exclude: /node_modules/, ** temp bug-fix discussed in lecture
         use: 'babel-loader',
       },
       {
